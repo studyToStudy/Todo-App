c1d07edb0fcd938eb4751b18476f88990b6bca2f
leejunghoo515@naver.com <leejunghoo515@naver.com>
2022-08-15 18:38:35 +0900

    항목 추가 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState, useRef, useCallback } from 'react'
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import TodoTemplate from './components/TodoTemplate'
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'First TodoList',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'React Practice',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱',
+      checked: false,
+    },
+  ]);
+  //ref 사용 변수 담기
+  const nextId = useRef(4);
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      }
+      setTodos(todos.concat(todo))
+      nextId.current += 1; //nextId 1씩 더하기
+    },
+    [todos],
+  )
   return (
-    <div>Todo App 만들기</div>
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
+    </TodoTemplate>
   )
 }
 
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,29 @@
+import { useState, useCallback } from 'react';
 import { MdDoneOutline } from 'react-icons/md'
 import './TodoInsert.scss';
 
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+    const [value, setValue] = useState('');
+
+    const onChagne = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(e => {
+        onInsert(value);
+        setValue(''); //value값 초기화 
+        e.preventDefault();
+    },
+        [onInsert, value],
+    );
     return (
-        <form className='TodoInsert'>
-            <input placeholder='할 일을 입력하세요' />
+        <form className='TodoInsert' onSubmit={onSubmit}>
+            <input
+                placeholder='할 일을 입력하세요'
+                value={value}
+                onChange={onChagne}
+            />
             <button type='submit'>
                 <MdDoneOutline />
             </button>
